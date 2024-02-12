5b44fbbf8cfc1e028fded1574c7838c162bf1ce4
Impavide <143338094+impavide@users.noreply.github.com>
2024-02-12 04:29:52 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	token: "", //token du bot manager
+    prefix: "", //votre prefix
+	owner: [ "", "" ], //votre id discord
+    logs: "", //votre Webhook pour les logs.
+    role: "" //le role whitelit pourr avoir accès au manager
+}
